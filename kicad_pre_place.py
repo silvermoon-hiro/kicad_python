bdf672ba47650e7d592e81e58280bced4aa25427
silvermoon-hiro <silvermoon-hiro@users.noreply.github.com>
2016-03-21 17:19:02 +0900

    Added support for scaling, origin and relocation

--- kicad_pre_place.py
+++ kicad_pre_place.py
@@ -1,8 +1,21 @@
-# coding: utf-8
-# KiCad footprint pre place tool (2016-01-21)
+﻿# coding: utf-8
+# KiCad footprint pre place tool (2016-01-30)
+# 
+# This script places footprints based on the XY coordinate
+# of Eeschema's schematic components.
 
+
+# import sys
 import codecs
 import pcbnew
+
+def input_num():
+    while True:
+        try:
+            v = float(sys.stdin.readline())
+            return v
+        except ValueError:
+            print 'Value Error!'
 
 comp_flg    = False    # For Detect $Comp section
 qty         = -1       # Comp counter
@@ -11,7 +24,7 @@ sch_comp    = []       # All Comp info (expect PWR-Symbol)
 sch_comp_xy = []       # Comp Location List
 pcb_comp_xy = []       # pcb location
 
-scl_size  = float(0.5)  # Scaling for pcb
+scl_size  = float(0.4)  # Scaling for pcb
 offset_x  = float(2000) # PCB Place offset X(mils)
 offset_y  = float(2000) #                  Y(mils)
 
@@ -19,8 +32,8 @@ pcb      = pcbnew.GetBoard()        # Get pcb all info
 pcb_name = pcb.GetFileName()        # current pcb file name
 sch_name = pcb_name.replace('.kicad_pcb','.sch') # Generate Sch file name
 
-# org_aux  = str(pcb.GetAuxOrigin())  # fab. origin(gerber etc.) and conv string (non use)
-# org_grid = str(pcb.GetGridOrigin()) # grid origin and conv string (non use)
+org_aux  = pcb.GetAuxOrigin()  # fab. origin(gerber etc.) and conv string (non used)
+org_grid = pcb.GetGridOrigin() # grid origin and conv string (non used)
 
 #Start Sch operation
 f   = codecs.open(sch_name,'r','utf-8') # Sch file reading (utf-8)
@@ -48,7 +61,7 @@ for line in sch: # reading each sch lines...
     if comp_flg :   # from $Comp to $EndComp
         
         if not line.startswith('$Comp') :  # except 1st line($Comp)
-            sch_comp[qty].append(line) # add comp all info
+            sch_comp[qty].append(line)     # add comp all info
                         
             # ex) sch_comp[0][0] <- 'L 74AC04 U2'
             #             [0][1] <- 'U 1 1 512E0139'
@@ -71,13 +84,13 @@ for c_line in sch_comp:
             xy_raw = [ref[2],float(xy[1]),float(xy[2]),angle] # make Ref,x,y list
             sch_comp_xy.append(xy_raw)
             
-            # ex) sch_comp[0][0] <- R1
-            #             [0][1] <- X(mils)
-            #             [0][2] <- Y(mils)
-            #             [0][3] <- orientation matrix
+            # ex) sch_comp_xy[0][0] <- R1
+            #                [0][1] <- X(mils)
+            #                [0][2] <- Y(mils)
+            #                [0][3] <- orientation matrix
             #
-            #             [1][0] <- C1
-            #                     .....
+            #                [1][0] <- C1
+            #                        .....
 
 # End Sch operation
 
@@ -87,17 +100,52 @@ for c_line in sch_comp:
 
 # Start PCB operation
 
-for posi in sch_comp_xy:
+place_flg = 'n'
+judge = 'n'
 
-    posi[1] = int(posi[1] * scl_size) # convert xy Sch --> PCB (scaling)
-    posi[2] = int(posi[2] * scl_size)
+while place_flg == 'n' :
 
-    module = pcb.FindModuleByReference(posi[0])               # Get Footprint info
-    module.SetPosition(pcbnew.wxPointMils(posi[1] , posi[2])) # Move abs x,y(mils)
-    module.Move(pcbnew.wxPointMils(offset_x , offset_y))        # Move inc(offset from origin, mils)
-
-print '''
-
-    Place Completed, Please Refresh Screen.
-
+    if judge != 'n' :
+        pass
+        
+    else :
+        print 'scale = ' + str(scl_size)
+        print 'offset x,y(mil) = ' + '(' +str(offset_x) + ',' + str(offset_y) + ')'
+        
+        for posi in sch_comp_xy:
+        
+            pcb_x = int(posi[1] * scl_size) # convert xy Sch --> PCB (scaling)
+            pcb_y = int(posi[2] * scl_size)
+        
+            module = pcb.FindModuleByReference(posi[0])           # Get Footprint info
+            module.SetPosition(pcbnew.wxPointMils(pcb_x , pcb_y)) # Move abs x,y(mils)
+            module.Move(pcbnew.wxPointMils(offset_x , offset_y))  # Move inc(offset from origin, mils)
+        
+        print '''
+    Place Finished , Press[F3] Key(Redraw).
 '''
+        
+        
+        
+    print 'Is this floor plan all right(y/n,a:abort)?' # Layout check...
+    
+    judge = sys.stdin.readline()
+    
+    if judge == 'y':          # Layout OK
+        print 'Place Completed.'
+        place_flg = 'y'
+        
+    elif judge == 'n':        # Change place scale and origin
+        print 'place scale = '
+        scl_size  = input_num()
+        print 'offset x(mil) = '
+        offset_x  = input_num()
+        print 'offset y(mil) = '
+        offset_y  = input_num()
+    
+    elif judge == 'a':        # abort
+        print 'Aborted.'
+        place_flg = 'a'
+    
+    else :
+        pass
